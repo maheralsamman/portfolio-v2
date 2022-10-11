bfd5d0a2b2ebfc747fb5d90edd20a9da805a48e0
maheralsamman <supermaher22@gmail.com>
2022-10-11 20:31:36 +0200

    add buttons, links and styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Link from 'next/link'
 
 type Props = {};
 
@@ -17,14 +18,38 @@ function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <img 
-      className="relative rounded-full h-32 w-32 mx-auto object-cover"
-      src="https://media.istockphoto.com/vectors/happy-handsome-man-showing-thumbs-up-concept-illustration-in-cartoon-vector-id980239992?k=20&m=980239992&s=612x612&w=0&h=xAzgjGwK4PPI3k6ZMnPT-I-_BYjeIGwwIf-OgTYMZoc=" 
-      alt="my picture" />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://media.istockphoto.com/vectors/happy-handsome-man-showing-thumbs-up-concept-illustration-in-cartoon-vector-id980239992?k=20&m=980239992&s=612x612&w=0&h=xAzgjGwK4PPI3k6ZMnPT-I-_BYjeIGwwIf-OgTYMZoc="
+        alt="my picture"
+      />
+
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+            <Link href="#about">
+        <button className="heroButton">About</button>
+            </Link>
+            <Link href="#experience">
+
+        <button className="heroButton">Experience</button>
+            </Link>
+            <Link href="#skills">
+
+        <button className="heroButton">Skills</button>
+            </Link>
+            <Link href="#projects">
+
+        <button className="heroButton">Projects</button>
+            </Link>
+        </div>
+      </div>
     </div>
   );
 }
