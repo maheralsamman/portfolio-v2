61353c35eefd31241933f0dd6310aaefa9cb7ae6
maheralsamman <supermaher22@gmail.com>
2022-10-11 19:42:31 +0200

    add header

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Head>
+        <title>Maher's Portfolio</title>
+      </Head>
+      <Header />
+      <section id="hero" className="snap-center">
+      <Hero />
+      </section>
+    </div>
+
+    /*
+
+    hero about
+    experinces
+    skills
+    projects
+    contact me */
+  );
+};
+
+export default Home;
