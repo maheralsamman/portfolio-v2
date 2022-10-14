abc3dc5900b1f8637fa34e32ebc2ce2e4ede7e21
maheralsamman <supermaher2@gmail.com>
2022-10-14 10:53:48 +0200

    add projects

---
+++ components/Projects.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+type Props = {};
+
+const Projects = (props: Props) => {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <img
+              src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png"
+              alt=""
+            />
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#f7ab0a]/50">
+                  Case Study {i + 1} of {projects.length}:
+                </span>{" "}
+                UPS clone
+              </h4>
+              <p>
+                Netflix 2.0 app that has a Log In and Log Out Authentication
+                with Google. It has a beautiful Home Screen with all the movies
+                looking just like Netflix. There is also a subscription page
+                where you can see your active monthly subscription. We also use
+                Stripe Payments for the monthly Netflix Subscriptions!
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
+    </div>
+  );
+};
+
+export default Projects;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -26,11 +27,13 @@ const Home: NextPage = () => {
       <section id="skills" className="snap-start">
         <Skills />
       </section>
+
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
     </div>
 
     /*
-    experinces
-    skills
     projects
     contact me */
   );
